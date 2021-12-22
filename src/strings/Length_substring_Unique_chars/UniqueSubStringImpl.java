834469a1ec9a1fbae727fca3a9c995b03e3a63f6
mmutawe <mahmoudmutawe@gmail.com>
2021-12-22 12:57:04 -0600

    using stringbuilder rather than string to imporove performance

--- src/strings/Length_substring_Unique_chars/UniqueSubStringImpl.java
+++ src/strings/Length_substring_Unique_chars/UniqueSubStringImpl.java
@@ -6,16 +6,16 @@ public class UniqueSubStringImpl implements UniqueSubString {
     // Space: k , Time: O(n * k) - where k is the length of substr
     @Override
     public int lengthOfLongestSubstringV1(String str) {
-        String subStr = "";
+        StringBuilder subStr = new StringBuilder();
         int maxSubstrLength = 0;
         for (int i = 0; i < str.length(); i++) {
             for (int j = 0; j < subStr.length(); j++) {
                 if (str.charAt(i) == subStr.charAt(j)) {
-                    subStr = subStr.substring(j + 1);
+                    subStr = subStr.delete(0, j+1);
                     break;
                 }
             }
-            subStr += str.charAt(i);
+            subStr.append(str.charAt(i));
 
             if (subStr.length() > maxSubstrLength) {
                 maxSubstrLength = subStr.length();
