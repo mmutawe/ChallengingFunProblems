b9d96cb8598ab1b707ea6517a9eebb1c55718180
mmutawe <mahmoudmutawe@gmail.com>
2021-12-24 14:53:09 -0600

    refactor comments

--- src/strings/validate_almost_palindrome/Test.java
+++ src/strings/validate_almost_palindrome/Test.java
@@ -15,31 +15,31 @@ public class Test {
 
         System.out.println("* Palendrome Validator:");
         String format = "%-15s%s";
-//
-//        System.out.printf(format, STR_TEST_SAMPLE_6
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_6) + "\n");
-//
-//        System.out.printf(format, STR_TEST_SAMPLE_7
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_7) + "\n");
-//
-//        System.out.printf(format, STR_TEST_SAMPLE_9
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_9) + "\n");
-//
-//        System.out.printf(format, STR_TEST_SAMPLE_10
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_10) + "\n");
-//
-//        System.out.printf(format, STR_TEST_SAMPLE_11
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_11) + "\n");
-//
-//        System.out.printf(format, STR_TEST_SAMPLE_12
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_12) + "\n");
+
+        System.out.printf(format, STR_TEST_SAMPLE_6
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_6) + "\n");
+
+        System.out.printf(format, STR_TEST_SAMPLE_7
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_7) + "\n");
+
+        System.out.printf(format, STR_TEST_SAMPLE_9
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_9) + "\n");
+
+        System.out.printf(format, STR_TEST_SAMPLE_10
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_10) + "\n");
+
+        System.out.printf(format, STR_TEST_SAMPLE_11
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_11) + "\n");
+
+        System.out.printf(format, STR_TEST_SAMPLE_12
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_12) + "\n");
 
         System.out.printf(format, STR_TEST_SAMPLE_13
                 , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_13) + "\n");
 
-//        System.out.printf(format, STR_TEST_SAMPLE_3
-//                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_3) + "\n");
-//
+        System.out.printf(format, STR_TEST_SAMPLE_3
+                , " --> " + almostPalindromeValidator.isAlmostPalindromeV1(STR_TEST_SAMPLE_3) + "\n");
+
 
     }
 
