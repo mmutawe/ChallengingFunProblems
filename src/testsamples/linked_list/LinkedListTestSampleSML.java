f50c1fb88028078b715030ff5ac92ad9bf847fcf
mmutawe <mahmoudmutawe@gmail.com>
2022-01-01 10:56:22 -0600

    init LinkedListTestSampleSML & fix ListNode constructors

---
+++ src/testsamples/linked_list/LinkedListTestSampleSML.java
@@ -0,0 +1,19 @@
+package testsamples.linked_list;
+
+import linked_lists.reverse_linked_list.ListNode;
+
+public class LinkedListTestSampleSML {
+
+    public static final ListNode LINKLIST_TEST_SAMPLE_NULL = null;
+    public static final ListNode LINKLIST_TEST_SAMPLE_NO_VALUES = new ListNode();
+
+    public static final ListNode LINKLIST_TEST_SAMPLE_1 = new ListNode(1);
+
+    public static final ListNode LINKLIST_TEST_SAMPLE_2 =
+            new ListNode(1,
+                    new ListNode(2,
+                            new ListNode(3,
+                                    new ListNode(4,
+                                            new ListNode(5)))));
+
+}
