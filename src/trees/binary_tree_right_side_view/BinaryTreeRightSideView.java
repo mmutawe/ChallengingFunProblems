8942d36538ad496892b6c162f79432c85b08ec45
mmutawe <mahmoudmutawe@gmail.com>
2022-02-02 08:18:14 -0600

    add description for BinaryTreeRightSideView problem

--- src/trees/binary_tree_right_side_view/BinaryTreeRightSideView.java
+++ src/trees/binary_tree_right_side_view/BinaryTreeRightSideView.java
@@ -3,7 +3,20 @@ package trees.binary_tree_right_side_view;
 import trees.shared.TreeNode;
 
 import java.util.List;
-
+// Given:
+//      A binary tree
+// Constrains:
+//      - if null return null
+//
+// TODO - Required:
+//      Return an array of the values of the nodes you can see from the right side of tree
+//      ordered from top to bottom
+//      Ex:
+//          ___(1)___
+//      ___2___     (3)     ---> ans: { 1, 3, 5, 6, 7 }
+//     4__    (5)
+//       (6)_
+//          (7)
 public interface BinaryTreeRightSideView {
     List<Integer> rightSideView(TreeNode root);
 }
